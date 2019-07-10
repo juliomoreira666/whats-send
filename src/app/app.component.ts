716e390c30245e9c69242fb01c7effc6086a45e7
Júlio Moreira <julio.moreira666@gmail.com>
2019-07-09 22:53:02 -0300

    Test inclusion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'whats-send';
+  linkApiWhats: string;
+  ddd: number;
+  numero: number;
+  mensagem: string;
+  sendMensagem() {
+    this.mensagem.replace(" ", "%20");
+    this.mensagem.replace(",", "%2C");
+    this.linkApiWhats = `https://api.whatsapp.com/send?phone=55${this.ddd}${
+      this.numero
+    }&text=${this.mensagem}`;
+    window.location.href = this.linkApiWhats;
+  }
 }
